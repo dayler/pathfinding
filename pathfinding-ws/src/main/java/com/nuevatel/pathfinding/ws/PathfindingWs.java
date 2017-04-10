81d96b88bbb7afb60b3581093c0ee3c7fdafbf73
Ariel Salazar <ariel.salazar@nuevatel.com>
2017-04-10 01:47:50 -0400

    Add docs.

--- pathfinding-ws/src/main/java/com/nuevatel/pathfinding/ws/PathfindingWs.java
+++ pathfinding-ws/src/main/java/com/nuevatel/pathfinding/ws/PathfindingWs.java
@@ -9,18 +9,11 @@ import javax.jws.WebService;
 
 import com.nuevatel.pathfinding.ws.domain.PathResponse;
 
-//import javax.jws.soap.SOAPBinding;
-//import javax.jws.soap.SOAPBinding.Style;
-//import javax.jws.soap.SOAPBinding.Use;
-//import javax.xml.ws.BindingType;
-
 /**
  * <p>The PathfindingWS class.</p>
  * <p>NuevaTel PCS de Bolivia S.A. (c) 2017</p>
  * <p>El contenido de este archivo esta clasificado como: </p>
  * <p>INFORMACION DE CONFIDENCIALIDAD ALTA </p>
- * 
- * TODO
  * 
  * @author asalazar
  * @version 1.0 Apr 8, 2017
@@ -28,8 +21,6 @@ import com.nuevatel.pathfinding.ws.domain.PathResponse;
  *
  */
 @WebService(name="PathfindingWSPort", wsdlLocation="/pathfinding.wsdl")
-//@BindingType(value = "http://java.sun.com/xml/ns/jaxws/2003/05/soap/bindings/HTTP/")
-//@SOAPBinding(style = Style.DOCUMENT, use = Use.LITERAL)
 public interface PathfindingWs {
     
     @WebMethod(operationName="getPath")
